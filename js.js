a590254c906e90e1b6da37539a7a167a07b4bb99
Antares-ProgramIng <140986762+Antares-ProgramIng@users.noreply.github.com>
2023-08-11 01:02:01 -0300

    Repaired Div Message bug

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 let energia = 0;
 const contenedor = document.getElementById("container");
+const controller = new AbortController;
+
+
 
   function giveCoffe() {
+    controller.abort();
     const imagen = document.getElementById("Bugs");
     alert("Le has dado un cafe a Bugs! \nAhora puedes saludarlo!");
     imagen.setAttribute("src", "Imagenes/Despierto.png")
@@ -9,6 +13,7 @@ const contenedor = document.getElementById("container");
   };
 
   function showAlert() {
+    controller.abort();
     if (energia > 0){
         alert("Que hay de nuevo viejo?");
     }else{
@@ -17,6 +22,10 @@ const contenedor = document.getElementById("container");
   };
 
   contenedor.addEventListener("click", function(e){
-    alert("Te saluda el Div! \nPero no soy Bugs! \nIntenta saludar a Bugs!");
-  });
+        alert("Te saluda el Div! \nPero no soy Bugs! \nIntenta saludar a Bugs!");
+  },
+  { signal: controller.signal },
+  );
 
+
+ 
