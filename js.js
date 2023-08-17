9767f0662c195920959120e622487b1e5155ffb8
Antares-ProgramIng <140986762+Antares-ProgramIng@users.noreply.github.com>
2023-08-17 11:02:10 -0300

    Final Final Last Commit

--- js.js
+++ js.js
@@ -1,10 +1,11 @@
 let energia = 0;
 const contenedor = document.getElementById("container");
 const controller = new AbortController;
+const coffeBtn = document.getElementById("btnCafe");
+const saludoBtn = document.getElementById("btnSaludo");
 
-
+/*
   function giveCoffe() {
-    controller.abort();
     const imagen = document.getElementById("Bugs");
     const btnSaludo = document.getElementById("btnSaludo")
     if (energia >= 5){
@@ -18,23 +19,53 @@ const controller = new AbortController;
         imagen.setAttribute("src", "Imagenes/Despierto.png")
         energia ++;
     }
-    
+    click.stopPropagation()
   };
-
+*/
+/*
   function showAlert() {
-    controller.abort();
     if (energia > 0){
-        alert("Que hay de nuevo viejo?");
-    }else{
-        alert("hmmm, bugs no esta de humor \nDeberias probar darle un cafecito...");
-    }
+      alert("Que hay de nuevo viejo?");
+  }else{
+      alert("hmmm, bugs no esta de humor \nDeberias probar darle un cafecito...");
+  }
+  click.stopPropagation()
   };
+*/
+
+  saludoBtn.addEventListener("click", function(e){
+
+    if (energia > 0){
+      alert("Que hay de nuevo viejo?");
+    }else{
+      alert("hmmm, bugs no esta de humor \nDeberias probar darle un cafecito...");
+    }
+    event.stopPropagation()
+  })
+
+  coffeBtn.addEventListener("click", function(e){
+    const imagen = document.getElementById("Bugs");
+    if (energia >= 5){
+        alert("Le has dado demasiado cafe a Bugs! \nYa no puedes saludarlo! \n Le dio amciedad!");
+        imagen.setAttribute("src", "Imagenes/MuchoCafe.png")
+        energia ++;
+        saludoBtn.disabled = true;
+    }else{
+        const imagen = document.getElementById("Bugs");
+        alert("Le has dado un cafe a Bugs! \nAhora puedes saludarlo!");
+        imagen.setAttribute("src", "Imagenes/Despierto.png")
+        energia ++;
+    }
+    event.stopPropagation()
+  })
 
   contenedor.addEventListener("click", function(e){
-        alert("Te saluda el Div! \nPero no soy Bugs! \nIntenta saludar a Bugs!");
-  },
+    const monigote = document.getElementById("Moni");
+           monigote.setAttribute("src", "Videos/Monigote-unscreen.gif")
+        setTimeout(() => {
+            monigote.setAttribute("src", "Videos/MonigoteFirstFrame.png")
+        }, 4170);
+    },
   { signal: controller.signal },
   );
 
-
- 
